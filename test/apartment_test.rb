3b963eb5ce98636c204f9168d58e8faea04cf4a2
Dani Coleman <danicoleman00@gmail.com>
2020-07-14 09:21:41 -0600

    Complete tests for apt class

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -19,14 +19,17 @@ class ApartmentTest < Minitest::Test
     assert_equal 1200, unit1.monthly_rent
     assert_equal 1, unit1.bathrooms
     assert_equal 1, unit1.bedrooms
+  end
+
+  def test_it_can_add_a_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+
     assert_equal nil, unit1.renter
+    
+    unit1.add_renter(renter1)
+
+    assert_equal renter1, unit1.renter
   end
 
 end
-
-
-# unit1.add_renter(renter1)
-# unit1.renter
-# # => #<Renter:0x00007fb3ee106ce8...>
-#
-# renter1 = Renter.new("Jessie")
