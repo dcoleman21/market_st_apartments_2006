66780a92f8738390c5b8a76d70f3f059c079a3d3
Dani Coleman <danicoleman00@gmail.com>
2020-07-14 10:48:56 -0600

    Add all tests for iteration 3; skipped

--- test/building_test.rb
+++ test/building_test.rb
@@ -61,23 +61,97 @@ class BuildingTest < Minitest::Test
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     renter1 = Renter.new("Aurora")
     renter2 = Renter.new("Tim")
-
+    
     building.add_unit(unit1)
     building.add_unit(unit2)
 
     unit1.add_renter(renter1)
     unit2.add_renter(renter2)
-    # require "pry"; binding.pry
 
     assert_equal 1099.5, building.average_rent
   end
 
+  def test_it_starts_with_no_rented_units
+    skip
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    assert_equal [], building.rented_units
+  end
+
+  def test_it_can_get_rented_units
+    skip
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    unit2.add_renter(renter1)
+
+    assert_equal [unit2], building.rented_units
+  end
+
+  def test_it_can_get_renter_with_hightest_rent
+    skip
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    renter2 = Renter.new("Jessie")
+    renter3 = Renter.new("Max")
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    unit2.add_renter(renter1)
+    unit1.add_renter(renter2)
+    unit3.add_renter(renter3)
+
+    assert_equal renter1, building.renter_with_highest_rent
+    assert_equal renter2, building.renter_with_highest_rent
+    assert_equal renter2, building.renter_with_highest_rent
+  end
+
+  def test_it_can_get_units_by_number_of_bedrooms
+    skip
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    renter2 = Renter.new("Jessie")
+    renter3 = Renter.new("Max")
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    building.add_unit(unit4)
+
+    expected = {
+      3 => ["D4"],
+      2 => ["B2", "C3"],
+      1 => ["A1"]
+    }
+
+    assert_equal expected, building.units_by_number_of_bedrooms
+  end
+
 end
-
-
-
-
-
-
-# building.average_rent
-# # => 1099.5
