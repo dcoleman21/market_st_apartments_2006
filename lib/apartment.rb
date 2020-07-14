73e8afd7ad2d52b97d8b1fff07b4280946d54c53
Dani Coleman <danicoleman00@gmail.com>
2020-07-14 09:15:14 -0600

    Initialize apt class

---
+++ lib/apartment.rb
@@ -0,0 +1,16 @@
+class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
+  def initialize(apt_param)
+    @number       = apt_param[:number]
+    @monthly_rent = apt_param[:monthly_rent]
+    @bathrooms    = apt_param[:bathrooms]
+    @bedrooms     = apt_param[:bedrooms]
+    @renter       = nil 
+  end
+
+end
